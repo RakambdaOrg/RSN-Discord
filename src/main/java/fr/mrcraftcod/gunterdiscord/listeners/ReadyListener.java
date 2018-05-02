5d7f05e077fdb0d50a5f058a514663d862ac9ff4
Thomas Couchoud <zerderr@gmail.com>
2018-05-02 18:37:35 +0200

    Clean trombi

--- src/main/java/fr/mrcraftcod/gunterdiscord/listeners/ReadyListener.java
+++ src/main/java/fr/mrcraftcod/gunterdiscord/listeners/ReadyListener.java
@@ -1,8 +1,15 @@
 package fr.mrcraftcod.gunterdiscord.listeners;
 
+import fr.mrcraftcod.gunterdiscord.settings.NoValueDefinedException;
+import fr.mrcraftcod.gunterdiscord.settings.configs.PhotoChannelConfig;
+import fr.mrcraftcod.gunterdiscord.utils.Log;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.Event;
 import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.hooks.EventListener;
+import java.io.InvalidClassException;
+import java.time.OffsetDateTime;
 
 /**
  * Created by Thomas Couchoud (MrCraftCod - zerderr@gmail.com) on 09/04/2018.
@@ -12,12 +19,34 @@ import net.dv8tion.jda.core.hooks.EventListener;
  */
 public class ReadyListener implements EventListener
 {
+	private int WAIT_TIME = 3600;
+	
 	@Override
 	public void onEvent(Event event)
 	{
-		if (event instanceof ReadyEvent)
+		if(event instanceof ReadyEvent)
 		{
-		
+			Thread t = new Thread(() -> {
+				try
+				{
+					TextChannel trombiChannel = event.getJDA().getTextChannelById(new PhotoChannelConfig().getLong());
+					//noinspection InfiniteLoopStatement
+					while(true)
+					{
+						for(Message message : trombiChannel.getIterableHistory().cache(false))
+							if(message.getCreationTime().isBefore(OffsetDateTime.now().minusDays(1)))
+								message.delete().queue();
+						
+						Thread.sleep(WAIT_TIME * 1000);
+					}
+				}
+				catch(InvalidClassException | NoValueDefinedException | InterruptedException e)
+				{
+					Log.error("Error getting photo channel", e);
+				}
+			});
+			t.setDaemon(true);
+			t.start();
 		}
 	}
 }
