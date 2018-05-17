ceaf3f8ac86d94600401bd4680939c616f738ef9
Thomas Couchoud <zerderr@gmail.com>
2018-05-17 21:48:40 +0200

    Binary fix

--- src/main/java/fr/mrcraftcod/gunterdiscord/commands/BinaryCommand.java
+++ src/main/java/fr/mrcraftcod/gunterdiscord/commands/BinaryCommand.java
@@ -28,8 +28,23 @@ public class BinaryCommand extends BasicCommand
 		if(Objects.equals(args.poll(), "t"))
 			Actions.reply(event, Arrays.stream(args.stream().collect(Collectors.joining(" ")).replace(" ", "").split("(?<=\\G.{8})")).map(s -> "" + ((char) Integer.parseInt(s, 2))).collect(Collectors.joining()));
 		else
-			Actions.reply(event, args.stream().collect(Collectors.joining(" ")).chars().mapToObj(i -> Integer.toBinaryString((char)i)).collect(Collectors.joining()));
-		return CommandResult.SUCCESS;
+			Actions.reply(event, binaryToText()args.stream().collect(Collectors.joining(" ")).chars().mapToObj(i -> Integer.toBinaryString((char) i)).collect(Collectors.joining())); return CommandResult.SUCCESS;
+	}
+	
+	private static String binaryToText(String input)
+	{
+		byte[] bytes = input.getBytes();
+		StringBuilder binary = new StringBuilder();
+		for(byte b : bytes)
+		{
+			int val = b;
+			for(int i = 0; i < 8; i++)
+			{
+				binary.append((val & 128) == 0 ? 0 : 1);
+				val <<= 1;
+			}
+		}
+		return binary.toString();
 	}
 	
 	@Override
