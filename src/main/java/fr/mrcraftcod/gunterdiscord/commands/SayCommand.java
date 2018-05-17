7ee156f674b0ee3a747ad54f622d80a8f9970b25
Thomas Couchoud <zerderr@gmail.com>
2018-05-17 21:05:33 +0200

    Say command

---
+++ src/main/java/fr/mrcraftcod/gunterdiscord/commands/SayCommand.java
@@ -0,0 +1,65 @@
+package fr.mrcraftcod.gunterdiscord.commands;
+
+import fr.mrcraftcod.gunterdiscord.commands.generic.BasicCommand;
+import fr.mrcraftcod.gunterdiscord.commands.generic.CallableCommand;
+import fr.mrcraftcod.gunterdiscord.commands.generic.CommandResult;
+import fr.mrcraftcod.gunterdiscord.utils.Actions;
+import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Thomas Couchoud (MrCraftCod - zerderr@gmail.com) on 12/04/2018.
+ *
+ * @author Thomas Couchoud
+ * @since 2018-04-12
+ */
+@CallableCommand
+public class SayCommand extends BasicCommand
+{
+	@Override
+	public String getCommandUsage()
+	{
+		return super.getCommandUsage() + " <message>";
+	}
+	
+	@Override
+	public int getScope()
+	{
+		return ChannelType.TEXT.getId();
+	}
+	
+	@Override
+	public String getName()
+	{
+		return "Say";
+	}
+	
+	@Override
+	public List<String> getCommand()
+	{
+		return List.of("say");
+	}
+	
+	@Override
+	public String getDescription()
+	{
+		return "Envoie un message";
+	}
+	
+	@Override
+	public CommandResult execute(MessageReceivedEvent event, LinkedList<String> args) throws Exception
+	{
+		super.execute(event, args);
+		Actions.reply(event, args.stream().collect(Collectors.joining(" ")));
+		return CommandResult.SUCCESS;
+	}
+	
+	@Override
+	public AccessLevel getAccessLevel()
+	{
+		return AccessLevel.ADMIN;
+	}
+}
