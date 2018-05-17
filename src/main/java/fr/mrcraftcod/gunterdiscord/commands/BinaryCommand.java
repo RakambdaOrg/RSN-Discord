22a93369ec97250c4b874092507529502c3705d7
Thomas Couchoud <zerderr@gmail.com>
2018-05-17 21:29:13 +0200

    Binary command

---
+++ src/main/java/fr/mrcraftcod/gunterdiscord/commands/BinaryCommand.java
@@ -0,0 +1,64 @@
+package fr.mrcraftcod.gunterdiscord.commands;
+
+import fr.mrcraftcod.gunterdiscord.commands.generic.BasicCommand;
+import fr.mrcraftcod.gunterdiscord.commands.generic.CallableCommand;
+import fr.mrcraftcod.gunterdiscord.commands.generic.CommandResult;
+import fr.mrcraftcod.gunterdiscord.utils.Actions;
+import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Thomas Couchoud (MrCraftCod - zerderr@gmail.com) on 12/04/2018.
+ *
+ * @author Thomas Couchoud
+ * @since 2018-04-12
+ */
+@CallableCommand
+public class BinaryCommand extends BasicCommand
+{
+	@Override
+	public CommandResult execute(MessageReceivedEvent event, LinkedList<String> args) throws Exception
+	{
+		super.execute(event, args);
+		if(Objects.equals(args.poll(), "t"))
+			Actions.reply(event, Arrays.stream(args.stream().collect(Collectors.joining(" ")).split("(?<=\\G.{8})")).map(s -> "" + ((char) Integer.parseInt(s, 2))).collect(Collectors.joining()));
+		else
+			Actions.reply(event, args.stream().collect(Collectors.joining(" ")).chars().mapToObj(i -> Integer.toBinaryString((char)i)).collect(Collectors.joining()));
+		return CommandResult.SUCCESS;
+	}
+	
+	@Override
+	public AccessLevel getAccessLevel()
+	{
+		return AccessLevel.ADMIN;
+	}
+	
+	@Override
+	public int getScope()
+	{
+		return ChannelType.TEXT.getId();
+	}
+	
+	@Override
+	public String getName()
+	{
+		return "Binary";
+	}
+	
+	@Override
+	public List<String> getCommand()
+	{
+		return List.of("binary");
+	}
+	
+	@Override
+	public String getDescription()
+	{
+		return "Texte en binaire";
+	}
+}
--- src/main/java/fr/mrcraftcod/gunterdiscord/listeners/CommandsMessageListener.java
+++ src/main/java/fr/mrcraftcod/gunterdiscord/listeners/CommandsMessageListener.java
@@ -29,7 +29,7 @@ import java.util.stream.Collectors;
  */
 public class CommandsMessageListener extends ListenerAdapter
 {
-	public static final List<Command> commands = Arrays.asList(new AddPhotoCommand(), new DelPhotoCommand(), new PhotoCommand(), new HangmanCommand(), new QuizCommand(), new ReportCommand(), new SetConfigCommand(), new StopCommand(), new HelpCommand(), new AvatarCommand(), new NicknameCommand(), new SayCommand());
+	public static final List<Command> commands = Arrays.asList(new AddPhotoCommand(), new DelPhotoCommand(), new PhotoCommand(), new HangmanCommand(), new QuizCommand(), new ReportCommand(), new SetConfigCommand(), new StopCommand(), new HelpCommand(), new AvatarCommand(), new NicknameCommand(), new SayCommand(), new BinaryCommand());
 	
 	public CommandsMessageListener()
 	{
