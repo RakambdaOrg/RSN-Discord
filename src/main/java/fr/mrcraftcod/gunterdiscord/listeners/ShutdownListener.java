1d716b8159ea65f2d07fb585a07bf30285839410
Thomas Couchoud <zerderr@gmail.com>
2018-05-30 18:03:08 +0200

    Brix pause

--- src/main/java/fr/mrcraftcod/gunterdiscord/listeners/ShutdownListener.java
+++ src/main/java/fr/mrcraftcod/gunterdiscord/listeners/ShutdownListener.java
@@ -4,15 +4,9 @@ import fr.mrcraftcod.gunterdiscord.listeners.quiz.QuizListener;
 import fr.mrcraftcod.gunterdiscord.settings.Settings;
 import fr.mrcraftcod.gunterdiscord.utils.Log;
 import net.dv8tion.jda.core.JDA;
-import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.ShutdownEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import java.io.IOException;
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 import java.util.logging.Handler;
 
 /**
@@ -23,23 +17,23 @@ import java.util.logging.Handler;
  */
 public class ShutdownListener extends ListenerAdapter
 {
-	private final ScheduledExecutorService service;
+	// private final ScheduledExecutorService service;
 	
 	public ShutdownListener(JDA jda)
 	{
-		final List<String> messages = List.of("Hey %s, chat va bien ?", "Oh tu sais quoi %s ? Hier soir j'ai mangé des boobs à la framboise !", "La vache %s, je sais pas si je vais réussir à dormir ce soir, tu me tiens companie ?", "Bonsoir %s", "Bonjour %s", "Tu parles trop %s", "Surout n'oublie pas, sors protégé %s", "Hey %s tu peux me raconter une histoire ?", "Miaou %s", "Oh mince %s, je crois je t'ai pin, mince alors", "%s ? %s ? C'était pour savoir si tu dormais", "%s? Tu as mis où les somnifaires ?", "%s Réveille toi! Tu ronfles et c'est insuportable !", "%s, tu m'en veux de te pin ?", "%s, avoue tu regrettes tes everyone !", "%s tu sais, à la fin de cette punition tu vas me manquer :frowning2:", "%s, tu connais l'histoire de toto aux toilettes ?", "%s, j'ai retrouvé ta sextape, j'en fais quoi, je la publie ?", "%s mange 5 fruits et légumes par jour! (www.mangerbouger.com)", "%s ne prends pas ce somnifère avant d'avoir demandé conseil à ton spécialiste et avoir lu la notice d'embalage", "%s t'es un voilà !");
-		service = Executors.newSingleThreadScheduledExecutor();
-		service.scheduleAtFixedRate(() -> {
-			try
-			{
-				User user = jda.getUserById(288737124444798976L);
-				jda.getGuildById(448124547322085397L).getTextChannelById(448130342512230417L).sendMessageFormat(messages.get(ThreadLocalRandom.current().nextInt(messages.size())), user.getAsMention()).complete();
-			}
-			catch(Exception e)
-			{
-				Log.error("BRIX", e);
-			}
-		}, 1, 15, TimeUnit.MINUTES);
+		// final List<String> messages = List.of("Hey %s, chat va bien ?", "Oh tu sais quoi %s ? Hier soir j'ai mangé des boobs à la framboise !", "La vache %s, je sais pas si je vais réussir à dormir ce soir, tu me tiens companie ?", "Bonsoir %s", "Bonjour %s", "Tu parles trop %s", "Surout n'oublie pas, sors protégé %s", "Hey %s tu peux me raconter une histoire ?", "Miaou %s", "Oh mince %s, je crois je t'ai pin, mince alors", "%s ? %s ? C'était pour savoir si tu dormais", "%s? Tu as mis où les somnifaires ?", "%s Réveille toi! Tu ronfles et c'est insuportable !", "%s, tu m'en veux de te pin ?", "%s, avoue tu regrettes tes everyone !", "%s tu sais, à la fin de cette punition tu vas me manquer :frowning2:", "%s, tu connais l'histoire de toto aux toilettes ?", "%s, j'ai retrouvé ta sextape, j'en fais quoi, je la publie ?", "%s mange 5 fruits et légumes par jour! (www.mangerbouger.com)", "%s ne prends pas ce somnifère avant d'avoir demandé conseil à ton spécialiste et avoir lu la notice d'embalage", "%s t'es un voilà !");
+		// service = Executors.newSingleThreadScheduledExecutor();
+		// service.scheduleAtFixedRate(() -> {
+		// 	try
+		// 	{
+		// 		User user = jda.getUserById(288737124444798976L);
+		// 		jda.getGuildById(448124547322085397L).getTextChannelById(448130342512230417L).sendMessageFormat(messages.get(ThreadLocalRandom.current().nextInt(messages.size())), user.getAsMention()).complete();
+		// 	}
+		// 	catch(Exception e)
+		// 	{
+		// 		Log.error("BRIX", e);
+		// 	}
+		// }, 1, 15, TimeUnit.MINUTES);
 	}
 	
 	@Override
@@ -48,7 +42,7 @@ public class ShutdownListener extends ListenerAdapter
 		super.onShutdown(event);
 		try
 		{
-			service.shutdownNow();
+			// service.shutdownNow();
 			QuizListener.stopAll();
 			HangmanListener.stopAll();
 			Settings.save();
