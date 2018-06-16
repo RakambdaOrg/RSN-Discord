449d62367db5cc9e7f641ba78dd4b2bd3f879ee5
Thomas Couchoud <zerderr@gmail.com>
2018-06-16 22:34:40 +0200

    feature(command) add up command

---
+++ src/main/java/fr/mrcraftcod/gunterdiscord/commands/UpCommand.java
@@ -0,0 +1,62 @@
+package fr.mrcraftcod.gunterdiscord.commands;
+
+import fr.mrcraftcod.gunterdiscord.Main;
+import fr.mrcraftcod.gunterdiscord.commands.generic.BasicCommand;
+import fr.mrcraftcod.gunterdiscord.commands.generic.CommandResult;
+import fr.mrcraftcod.gunterdiscord.utils.Actions;
+import fr.mrcraftcod.gunterdiscord.utils.Utilities;
+import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import java.awt.*;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Thomas Couchoud (MrCraftCod - zerderr@gmail.com) on 12/04/2018.
+ *
+ * @author Thomas Couchoud
+ * @since 2018-04-12
+ */
+public class UpCommand extends BasicCommand
+{
+	@Override
+	public CommandResult execute(MessageReceivedEvent event, LinkedList<String> args) throws Exception
+	{
+		super.execute(event, args);
+		Actions.sendMessage(event.getTextChannel(), Utilities.buildEmbed(event.getAuthor(), Color.GREEN, "Uptime infos").addField("Dernier démarage (heure locale):", Main.bootTime.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME), false).addField("Temps écoulé", Duration.between(Main.bootTime, LocalDateTime.now()).toString(), false).build());
+		return CommandResult.SUCCESS;
+	}
+	
+	@Override
+	public int getScope()
+	{
+		return ChannelType.TEXT.getId();
+	}
+	
+	@Override
+	public List<String> getCommand()
+	{
+		return List.of("up");
+	}
+	
+	@Override
+	public String getName()
+	{
+		return "Uptime infos";
+	}
+	
+	@Override
+	public String getDescription()
+	{
+		return "Envoie des infos sur la durée de fonctionnement du bot";
+	}
+	
+	@Override
+	public AccessLevel getAccessLevel()
+	{
+		return AccessLevel.ALL;
+	}
+}
