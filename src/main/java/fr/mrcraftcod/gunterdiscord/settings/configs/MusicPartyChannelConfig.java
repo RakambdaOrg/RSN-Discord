8efee95df9efaea7ab634810cb3b0b33dc99f04c
Thomas COUCHOUD <zerderr@gmail.com>
2018-06-21 13:25:00 +0000

    Add new file

---
+++ src/main/java/fr/mrcraftcod/gunterdiscord/settings/configs/MusicPartyChannelConfig.java
@@ -0,0 +1,18 @@
+package fr.mrcraftcod.gunterdiscord.settings.configs;
+
+import fr.mrcraftcod.gunterdiscord.settings.configurations.SingleChannelConfiguration;
+
+/**
+ * Created by Thomas Couchoud (MrCraftCod - zerderr@gmail.com)
+ *
+ * @author Thomas Couchoud
+ * @since 2018-06-21
+ */
+public class MusicPartyChannelConfig extends SingleChannelConfiguration
+{
+	@Override
+	public String getName()
+	{
+		return "musicPartyChannel";
+	}
+}
