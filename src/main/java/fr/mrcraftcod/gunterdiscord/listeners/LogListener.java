7f9f48e93a46de5c232743c8b6fc957ad5eefd27
Thomas Couchoud <zerderr@gmail.com>
2018-07-29 18:11:28 +0200

    Add back logging

--- src/main/java/fr/mrcraftcod/gunterdiscord/listeners/LogListener.java
+++ src/main/java/fr/mrcraftcod/gunterdiscord/listeners/LogListener.java
@@ -1,6 +1,11 @@
 package fr.mrcraftcod.gunterdiscord.listeners;
 
+import fr.mrcraftcod.gunterdiscord.utils.Utilities;
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceGuildMuteEvent;
+import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.core.events.message.react.MessageReactionRemoveEvent;
+import net.dv8tion.jda.core.events.self.SelfUpdateNameEvent;
+import net.dv8tion.jda.core.events.user.update.UserUpdateNameEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import static fr.mrcraftcod.gunterdiscord.utils.log.Log.getLogger;
 
@@ -11,6 +16,58 @@ import static fr.mrcraftcod.gunterdiscord.utils.log.Log.getLogger;
  * @since 2018-05-06
  */
 public class LogListener extends ListenerAdapter{
+	@Override
+	public void onUserUpdateName(UserUpdateNameEvent event){
+		super.onUserUpdateName(event);
+		try{
+			getLogger(null).info("User {} changed name of {} `{}` to `{}`", Utilities.getUserToLog(event.getUser()), Utilities.getUserToLog(event.getEntity()), event.getOldName(), event.getNewName());
+		}
+		catch(NullPointerException ignored){
+		}
+		catch(Exception e){
+			getLogger(null).error("", e);
+		}
+	}
+	
+	@Override
+	public void onSelfUpdateName(SelfUpdateNameEvent event){
+		super.onSelfUpdateName(event);
+		try{
+			getLogger(null).info("User {} changed name `{}` to `{}`", Utilities.getUserToLog(event.getEntity()), event.getOldName(), event.getNewName());
+		}
+		catch(NullPointerException ignored){
+		}
+		catch(Exception e){
+			getLogger(null).error("", e);
+		}
+	}
+	
+	@Override
+	public void onMessageReactionAdd(MessageReactionAddEvent event){
+		super.onMessageReactionAdd(event);
+		try{
+			event.getReaction().getTextChannel().getMessageById(event.getMessageIdLong()).queue(m -> getLogger(event.getGuild()).info("New reaction {} from `{}` in {} on `{}` whose author is {}", event.getReaction().getReactionEmote().getName(), Utilities.getUserToLog(event.getUser()), event.getReaction().getTextChannel().getName(), m.getContentRaw().replace("\n", "{n}"), Utilities.getUserToLog(m.getAuthor())));
+		}
+		catch(NullPointerException ignored){
+		}
+		catch(Exception e){
+			getLogger(event.getGuild()).error("", e);
+		}
+	}
+	
+	@Override
+	public void onMessageReactionRemove(MessageReactionRemoveEvent event){
+		super.onMessageReactionRemove(event);
+		try{
+			event.getReaction().getTextChannel().getMessageById(event.getMessageIdLong()).queue(m -> getLogger(event.getGuild()).info("Reaction {} removed by `{}` in {} on `{}` whose author is {}", event.getReaction().getReactionEmote().getName(), Utilities.getUserToLog(event.getUser()), event.getReaction().getTextChannel().getName(), m.getContentRaw().replace("\n", "{n}"), Utilities.getUserToLog(m.getAuthor())));
+		}
+		catch(NullPointerException ignored){
+		}
+		catch(Exception e){
+			getLogger(event.getGuild()).error("", e);
+		}
+	}
+	
 	@Override
 	public void onGuildVoiceGuildMute(GuildVoiceGuildMuteEvent event){
 		super.onGuildVoiceGuildMute(event);
