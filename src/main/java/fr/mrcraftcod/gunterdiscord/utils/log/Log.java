c446499ca70dd557a1285d23b0fd04c551e6c355
Thomas Couchoud <zerderr@gmail.com>
2018-07-29 15:25:35 +0200

    Add missing class

---
+++ src/main/java/fr/mrcraftcod/gunterdiscord/utils/log/Log.java
@@ -0,0 +1,21 @@
+package fr.mrcraftcod.gunterdiscord.utils.log;
+
+import net.dv8tion.jda.core.entities.Guild;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.util.HashMap;
+import java.util.Objects;
+
+@SuppressWarnings("JavaDoc")
+public class Log{
+	private static final HashMap<Guild, Logger> LOGGERS = new HashMap<>();
+	
+	public static Logger getLogger(Guild g){
+		return LOGGERS.computeIfAbsent(g, g2 -> {
+			if(Objects.nonNull(g2)){
+				return LoggerFactory.getLogger(g2.getName());
+			}
+			return LoggerFactory.getLogger("No Guild");
+		});
+	}
+}
