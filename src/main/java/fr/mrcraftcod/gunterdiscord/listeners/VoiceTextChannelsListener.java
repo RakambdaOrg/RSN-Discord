4704a338d734090243ade29da142844196d2d87f
Thomas Couchoud <zerderr@gmail.com>
2018-08-01 18:07:38 +0200

    Update text-voice on channel move & add mods perms Closes #10

--- src/main/java/fr/mrcraftcod/gunterdiscord/listeners/VoiceTextChannelsListener.java
+++ src/main/java/fr/mrcraftcod/gunterdiscord/listeners/VoiceTextChannelsListener.java
@@ -1,11 +1,15 @@
 package fr.mrcraftcod.gunterdiscord.listeners;
 
+import fr.mrcraftcod.gunterdiscord.settings.configs.ModoRolesConfig;
 import fr.mrcraftcod.gunterdiscord.settings.configs.VoiceTextChannelsConfig;
 import fr.mrcraftcod.gunterdiscord.utils.Actions;
 import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceJoinEvent;
 import net.dv8tion.jda.core.events.guild.voice.GuildVoiceLeaveEvent;
+import net.dv8tion.jda.core.events.guild.voice.GuildVoiceMoveEvent;
+import net.dv8tion.jda.core.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import net.dv8tion.jda.core.requests.restaction.ChannelAction;
 import java.util.HashMap;
@@ -33,6 +37,9 @@ public class VoiceTextChannelsListener extends ListenerAdapter{
 					creator.setParent(event.getChannelJoined().getParent());
 					creator.setTopic("Channel pour les utilisateurs du vocal du même nom");
 					creator.addPermissionOverride(event.getGuild().getPublicRole(), List.of(), List.of(Permission.MESSAGE_READ));
+					for(Role role : new ModoRolesConfig(event.getGuild()).getAsList()){
+						creator.addPermissionOverride(role, List.of(Permission.MESSAGE_READ), List.of());
+					}
 					return (TextChannel) creator.complete();
 				});
 				
@@ -46,20 +53,41 @@ public class VoiceTextChannelsListener extends ListenerAdapter{
 	}
 	
 	@Override
-	public void onGuildVoiceLeave(GuildVoiceLeaveEvent event){
-		super.onGuildVoiceLeave(event);
+	public void onGuildVoiceMove(GuildVoiceMoveEvent event){
+		super.onGuildVoiceMove(event);
 		try{
-			TextChannel channel = CHANNELS.get(event.getChannelLeft().getIdLong());
-			if(Objects.nonNull(channel)){
-				channel.putPermissionOverride(event.getMember()).setDeny(Permission.MESSAGE_READ).queue();
-				if(event.getChannelLeft().getMembers().isEmpty()){
-					channel.delete().reason("No more users in the vocal").queue();
-					CHANNELS.remove(event.getChannelLeft().getIdLong());
-				}
-			}
+			leaveVoice(event);
 		}
 		catch(Exception e){
 			getLogger(event.getGuild()).error("", e);
 		}
 	}
+	
+	@Override
+	public void onGuildVoiceLeave(GuildVoiceLeaveEvent event){
+		super.onGuildVoiceLeave(event);
+		try{
+			leaveVoice(event);
+		}
+		catch(Exception e){
+			getLogger(event.getGuild()).error("", e);
+		}
+	}
+	
+	/**
+	 * Handle things when a user leaves a channel.
+	 *
+	 * @param event The event.
+	 */
+	private static void leaveVoice(GuildVoiceUpdateEvent event){
+		TextChannel channel = CHANNELS.get(event.getChannelLeft().getIdLong());
+		if(Objects.nonNull(channel)){
+			channel.putPermissionOverride(event.getMember()).setDeny(Permission.MESSAGE_READ).queue();
+			if(event.getChannelLeft().getMembers().isEmpty()){
+				channel.delete().reason("No more users in the vocal").queue();
+				CHANNELS.remove(event.getChannelLeft().getIdLong());
+			}
+		}
+	}
 }
+
