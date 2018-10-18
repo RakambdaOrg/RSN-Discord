21794aa03afbc4b500be98dc0eea99c50a157b38
Thomas COUCHOUD <zerderr@gmail.com>
2018-10-18 18:47:56 +0200

    Should fix #18

--- src/main/java/fr/mrcraftcod/gunterdiscord/listeners/AutoRolesListener.java
+++ src/main/java/fr/mrcraftcod/gunterdiscord/listeners/AutoRolesListener.java
@@ -1,9 +1,12 @@
 package fr.mrcraftcod.gunterdiscord.listeners;
 
 import fr.mrcraftcod.gunterdiscord.settings.configs.AutoRolesConfig;
+import fr.mrcraftcod.gunterdiscord.settings.configs.RemoveRoleConfig;
 import fr.mrcraftcod.gunterdiscord.utils.Actions;
 import net.dv8tion.jda.core.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import java.time.Duration;
+import java.util.Objects;
 import static fr.mrcraftcod.gunterdiscord.utils.log.Log.getLogger;
 
 /**
@@ -18,6 +21,18 @@ public class AutoRolesListener extends ListenerAdapter{
 		super.onGuildMemberJoin(event);
 		try{
 			Actions.giveRole(event.getUser(), new AutoRolesConfig(event.getGuild()).getAsList());
+			final var currentTime = System.currentTimeMillis();
+			final var config = new RemoveRoleConfig(event.getGuild());
+			final var userGuildConfig = config.getValue(event.getUser().getIdLong());
+			if(Objects.nonNull(userGuildConfig)){
+				for(final var roleID : userGuildConfig.keySet()){
+					final var diff = Duration.ofMillis(userGuildConfig.get(roleID) - currentTime);
+					final var role = event.getGuild().getRoleById(roleID);
+					if(!diff.isNegative()){
+						Actions.giveRole(event.getGuild(), event.getUser(), role);
+					}
+				}
+			}
 		}
 		catch(final Exception e){
 			getLogger(event.getGuild()).error("Error on user join", e);
