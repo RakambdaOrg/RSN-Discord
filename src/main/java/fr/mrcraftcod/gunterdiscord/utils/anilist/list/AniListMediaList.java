f85707282be96d3c3e73b28b687ae6597e8d344c
Thomas Couchoud <zerderr@gmail.com>
2018-10-24 22:11:01 +0200

    Add media URL in title

--- src/main/java/fr/mrcraftcod/gunterdiscord/utils/anilist/list/AniListMediaList.java
+++ src/main/java/fr/mrcraftcod/gunterdiscord/utils/anilist/list/AniListMediaList.java
@@ -70,7 +70,7 @@ public class AniListMediaList implements AniListDatedObject{
 	public void fillEmbed(final EmbedBuilder builder){
 		builder.setTimestamp(getDate().toInstant());
 		builder.setColor(getStatus().getColor());
-		builder.setTitle("List changed");
+		builder.setTitle("List changed", getMedia().getUrl());
 		builder.addField("List status", this.getStatus().toString(), true);
 		if(Objects.nonNull(getScore())){
 			builder.addField("Score", this.getScore() + "/100", true);
