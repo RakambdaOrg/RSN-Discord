88d5c17dd6c294b0f38490ee90cc628aca881469
Thomas COUCHOUD <zerderr@gmail.com>
2018-10-25 08:30:40 +0200

    Temp command

--- src/main/java/fr/mrcraftcod/gunterdiscord/runners/DisplayDailyStatsScheduledRunner.java
+++ src/main/java/fr/mrcraftcod/gunterdiscord/runners/DisplayDailyStatsScheduledRunner.java
@@ -50,7 +50,7 @@ public class DisplayDailyStatsScheduledRunner implements ScheduledRunner{
 					getLogger(guild).info("{}", stats);
 					final var builder = Utilities.buildEmbed(this.jda.getSelfUser(), Color.MAGENTA, "Participation of the " + date);
 					stats.entrySet().stream().sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue())).limit(10).forEachOrdered(e -> {
-						getLogger(guild).debug("A");
+						getLogger(guild).debug("A {}", e);
 						builder.addField(this.jda.getUserById(e.getKey()).getAsMention(), "Messages: " + e.getValue(), false);
 						getLogger(guild).debug("Z");
 					});
