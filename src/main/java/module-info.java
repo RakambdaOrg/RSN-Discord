a507decd4d95adde83b9968bd8d528db3b822985
Thomas Couchoud <thomas.couchoud@gmail.com>
2019-06-17 13:40:28 +0200

    Remove unused dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,7 +8,6 @@ open module fr.mrcraftcod.gunterdiscord {
 	requires java.desktop;
 	
 	requires org.json;
-	requires org.apache.commons.io;
 	requires org.apache.commons.lang3;
 	requires org.jetbrains.annotations;
 	requires fr.mrcraftcod.utils.http;
