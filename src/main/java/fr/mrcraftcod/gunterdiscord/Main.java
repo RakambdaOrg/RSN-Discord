ff2ce870f64602b7b36820879356c01ab3fafcdd
Thomas Couchoud <thomas.couchoud@gmail.com>
2019-07-12 22:15:54 +0200

    Show number of loaded settings

--- src/main/java/fr/mrcraftcod/gunterdiscord/Main.java
+++ src/main/java/fr/mrcraftcod/gunterdiscord/Main.java
@@ -5,12 +5,12 @@ import com.beust.jcommander.ParameterException;
 import fr.mrcraftcod.gunterdiscord.listeners.*;
 import fr.mrcraftcod.gunterdiscord.listeners.quiz.QuizListener;
 import fr.mrcraftcod.gunterdiscord.listeners.reply.ReplyMessageListener;
-import fr.mrcraftcod.gunterdiscord.settings.NewSettings;
 import fr.mrcraftcod.gunterdiscord.runners.DisplayDailyStatsScheduledRunner;
 import fr.mrcraftcod.gunterdiscord.runners.RemoveRolesScheduledRunner;
 import fr.mrcraftcod.gunterdiscord.runners.SaveConfigScheduledRunner;
 import fr.mrcraftcod.gunterdiscord.runners.anilist.AniListMediaUserListScheduledRunner;
 import fr.mrcraftcod.gunterdiscord.runners.anilist.AniListNotificationScheduledRunner;
+import fr.mrcraftcod.gunterdiscord.settings.NewSettings;
 import fr.mrcraftcod.gunterdiscord.utils.irc.TwitchIRC;
 import fr.mrcraftcod.gunterdiscord.utils.log.Log;
 import fr.mrcraftcod.gunterdiscord.utils.player.GunterAudioManager;
@@ -18,8 +18,6 @@ import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import javax.annotation.Nonnull;
 import javax.security.auth.login.LoginException;
 import java.io.FileInputStream;
@@ -39,7 +37,6 @@ import static fr.mrcraftcod.gunterdiscord.utils.log.Log.getLogger;
  * @since 2018-04-09
  */
 public class Main{
-	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 	public static final ZonedDateTime bootTime = ZonedDateTime.now();
 	private static final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
 	private static JDA jda;
@@ -51,13 +48,13 @@ public class Main{
 	 * @param args Not used.
 	 */
 	public static void main(@Nonnull final String[] args){
-		LOGGER.info("Starting bot version {}", getRSNBotVersion());
+		getLogger(null).info("Starting bot version {}", getRSNBotVersion());
 		final var parameters = new CLIParameters();
 		try{
 			JCommander.newBuilder().addObject(parameters).build().parse(args);
 		}
 		catch(final ParameterException e){
-			LOGGER.error("Failed to parse arguments", e);
+			getLogger(null).error("Failed to parse arguments", e);
 			e.usage();
 			return;
 		}
@@ -68,14 +65,14 @@ public class Main{
 				prop.load(is);
 			}
 			catch(final IOException e){
-				LOGGER.warn("Failed to read file {}", parameters.getConfigurationFile());
+				getLogger(null).warn("Failed to read file {}", parameters.getConfigurationFile());
 			}
 			prop.forEach((key, value) -> System.setProperty(key.toString(), value.toString()));
-			LOGGER.debug("Loaded {} properties from file", prop.keySet().size());
+			getLogger(null).debug("Loaded {} properties from file", prop.keySet().size());
 		}
 		
 		try{
-			LOGGER.info("Building JDA");
+			getLogger(null).info("Building JDA");
 			final var jdaBuilder = new JDABuilder(AccountType.BOT).setToken(System.getProperty("RSN_TOKEN"));
 			jdaBuilder.addEventListeners(new CommandsMessageListener());
 			// jdaBuilder.addEventListeners(new OnlyImagesMessageListener());
@@ -90,23 +87,23 @@ public class Main{
 			Log.setJDA(jda);
 			jda.awaitReady();
 			jda.getPresence().setActivity(Activity.playing("g?help for the help"));
-			
-			LOGGER.info("Creating runners");
+			getLogger(null).info("Loaded {} guild settings", jda.getGuilds().stream().map(NewSettings::getConfiguration).count());
+			getLogger(null).info("Creating runners");
 			final var scheduledRunners = List.of(new RemoveRolesScheduledRunner(jda), new AniListNotificationScheduledRunner(jda), new AniListMediaUserListScheduledRunner(jda), new SaveConfigScheduledRunner(), new DisplayDailyStatsScheduledRunner(jda));
 			for(final var scheduledRunner : scheduledRunners){
 				executorService.scheduleAtFixedRate(scheduledRunner, scheduledRunner.getDelay(), scheduledRunner.getPeriod(), scheduledRunner.getPeriodUnit());
 			}
-			LOGGER.info("Started");
+			getLogger(null).info("Started");
 		}
 		catch(final LoginException | InterruptedException e){
 			getLogger(null).error("Couldn't start bot", e);
 		}
 		
 		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-			LOGGER.info("Shutdown hook triggered");
+			getLogger(null).info("Shutdown hook triggered");
 			NewSettings.close();
 		}));
-		LOGGER.info("Shutdown hook registered");
+		getLogger(null).info("Shutdown hook registered");
 		
 		consoleHandler = new ConsoleHandler(jda);
 		consoleHandler.start();
@@ -133,7 +130,7 @@ public class Main{
 			properties.load(Main.class.getResource("/version.properties").openStream());
 		}
 		catch(final IOException e){
-			LOGGER.warn("Error reading version jsonConfigFile", e);
+			getLogger(null).warn("Error reading version jsonConfigFile", e);
 		}
 		return properties.getProperty("simulator.version", "Unknown");
 	}
