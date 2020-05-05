27c11296f4e26643db418d7e9e4e8cf72ec0bfd4
Thomas Couchoud <thomas.couchoud@gmail.com>
2020-05-05 13:21:02 +0200

    Fix music access

--- src/main/java/fr/raksrinana/rsndiscord/commands/music/MusicCommandComposite.java
+++ src/main/java/fr/raksrinana/rsndiscord/commands/music/MusicCommandComposite.java
@@ -2,14 +2,8 @@ package fr.raksrinana.rsndiscord.commands.music;
 
 import fr.raksrinana.rsndiscord.commands.generic.BotCommand;
 import fr.raksrinana.rsndiscord.commands.generic.CommandComposite;
-import fr.raksrinana.rsndiscord.settings.Settings;
-import fr.raksrinana.rsndiscord.settings.types.RoleConfiguration;
-import fr.raksrinana.rsndiscord.utils.Utilities;
 import lombok.NonNull;
-import net.dv8tion.jda.api.entities.Member;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 @BotCommand
 public class MusicCommandComposite extends CommandComposite{
@@ -28,11 +22,6 @@ public class MusicCommandComposite extends CommandComposite{
 		this.addSubCommand(new ShuffleMusicCommand(this));
 		this.addSubCommand(new MoveMusicCommand(this));
 		this.addSubCommand(new VolumeMusicCommand(this));
-	}
-	
-	@Override
-	public boolean isAllowed(final Member member){
-		return Objects.nonNull(member) && (Utilities.isTeam(member) || Settings.get(member.getGuild()).getDjRole().map(RoleConfiguration::getRole).filter(Optional::isPresent).map(Optional::get).map(role -> member.getRoles().contains(role)).orElse(false));
 	}
 	
 	@NonNull
