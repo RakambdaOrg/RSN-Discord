b4337a3c51c529bbacbd9e72a3390604a278656a
Thomas Couchoud <thomas.couchoud@gmail.com>
2020-05-21 23:13:38 +0200

    Use mentien instead of toString

--- src/main/java/fr/raksrinana/rsndiscord/commands/trombinoscope/GetCommand.java
+++ src/main/java/fr/raksrinana/rsndiscord/commands/trombinoscope/GetCommand.java
@@ -98,7 +98,7 @@ class GetCommand extends BasicCommand{
 				.findFirst()
 				.ifPresentOrElse(picture -> trombinoscope.getPicturesChannel()
 						.flatMap(ChannelConfiguration::getChannel)
-						.ifPresent(picturesChannel -> Actions.sendMessage(picturesChannel, event.getAuthor() + " requested a picture of " + target.getAsMention() + " (ID: `" + picture.getUuid() + "`)", null, false, message -> message.addFile(picture.getPath().toFile()))), () -> Actions.reply(event, "Picture not found", null));
+						.ifPresent(picturesChannel -> Actions.sendMessage(picturesChannel, event.getAuthor().getAsMention() + " requested a picture of " + target.getAsMention() + " (ID: `" + picture.getUuid() + "`)", null, false, message -> message.addFile(picture.getPath().toFile()))), () -> Actions.reply(event, "Picture not found", null));
 		return CommandResult.SUCCESS;
 	}
 	
