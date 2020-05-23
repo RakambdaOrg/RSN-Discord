fcb77d6ebe69b019d2dc0dc2fe48f3ab25a74e8c
Thomas Couchoud <thomas.couchoud@gmail.com>
2020-05-23 18:29:42 +0200

    Wall with parameter

--- src/main/java/fr/raksrinana/rsndiscord/commands/trombinoscope/WallCommand.java
+++ src/main/java/fr/raksrinana/rsndiscord/commands/trombinoscope/WallCommand.java
@@ -5,9 +5,12 @@ import fr.raksrinana.rsndiscord.commands.generic.Command;
 import fr.raksrinana.rsndiscord.commands.generic.CommandResult;
 import fr.raksrinana.rsndiscord.settings.Settings;
 import fr.raksrinana.rsndiscord.settings.guild.trombinoscope.Picture;
+import fr.raksrinana.rsndiscord.settings.types.ChannelConfiguration;
 import fr.raksrinana.rsndiscord.utils.Actions;
 import fr.raksrinana.rsndiscord.utils.log.Log;
 import lombok.NonNull;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import org.imgscalr.Scalr;
 import javax.imageio.ImageIO;
@@ -33,10 +36,17 @@ class WallCommand extends BasicCommand{
 		super(parent);
 	}
 	
+	@Override
+	public void addHelp(@NonNull Guild guild, @NonNull EmbedBuilder builder){
+		super.addHelp(guild, builder);
+		builder.addField("Mode", "Determines how the picture is created: [STRETCH, KEEP_ASPECT_RATIO]", false);
+	}
+	
 	@NonNull
 	@Override
 	public CommandResult execute(@NonNull final GuildMessageReceivedEvent event, @NonNull final LinkedList<String> args){
 		super.execute(event, args);
+		var mode = Optional.ofNullable(args.poll()).flatMap(PictureMode::fromString).orElse(PictureMode.STRETCH);
 		var trombinoscopeConfiguration = Settings.get(event.getGuild()).getTrombinoscope();
 		var userCount = trombinoscopeConfiguration.getUserCount();
 		var squareCountPerSide = (int) Math.ceil(Math.sqrt(userCount));
@@ -44,12 +54,12 @@ class WallCommand extends BasicCommand{
 		var bufferedImage = new BufferedImage(squarePixels * squareCountPerSide, squarePixels * squareCountPerSide, BufferedImage.TYPE_INT_RGB);
 		var g2d = bufferedImage.createGraphics();
 		var currentIndex = new AtomicInteger(0);
-		trombinoscopeConfiguration.getPictures().forEach((userId, pictures) -> {
+		trombinoscopeConfiguration.getPictures().entrySet().stream().parallel().forEach(entry -> {
 			var userIndex = currentIndex.getAndIncrement();
-			pictures.stream().skip(0).findFirst().ifPresent(picture -> {
+			entry.getValue().stream().skip(0).findFirst().ifPresent(picture -> {
 				int x = userIndex / squareCountPerSide;
 				int y = userIndex % squareCountPerSide;
-				drawImage(g2d, picture, squarePixels * x, squarePixels * y, squarePixels);
+				drawImage(g2d, picture, squarePixels * x, squarePixels * y, squarePixels, mode);
 			});
 		});
 		g2d.dispose();
@@ -57,10 +67,7 @@ class WallCommand extends BasicCommand{
 		try{
 			if(ImageIO.write(bufferedImage, "jpeg", imgOutputStream)){
 				try(var imgInputStream = new ByteArrayInputStream(imgOutputStream.toByteArray())){
-					// trombinoscopeConfiguration.getPicturesChannel().flatMap(ChannelConfiguration::getChannel).ifPresent(channel -> {
-					Optional.ofNullable(event.getGuild().getTextChannelById(436492330602594304L)).ifPresent(channel -> {
-						Actions.sendMessage(channel, "", null, false, action -> action.addFile(imgInputStream, System.currentTimeMillis() + ".jpg"));
-					});
+					trombinoscopeConfiguration.getPicturesChannel().flatMap(ChannelConfiguration::getChannel).ifPresent(channel -> Actions.sendMessage(channel, "Full trombinoscope with " + userCount + " users. (Requested by " + event.getAuthor().getAsMention() + ")", null, false, action -> action.addFile(imgInputStream, System.currentTimeMillis() + ".jpg")));
 				}
 			}
 		}
@@ -70,20 +77,23 @@ class WallCommand extends BasicCommand{
 		return CommandResult.SUCCESS;
 	}
 	
-	private void drawImage(Graphics2D g2d, Picture picture, int x, int y, int dim){
+	@Override
+	public @NonNull String getCommandUsage(){
+		return super.getCommandUsage() + " [mode]";
+	}
+	
+	private void drawImage(Graphics2D g2d, Picture picture, int x, int y, int dim, PictureMode mode){
 		try{
 			var image = ImageIO.read(picture.getPath().toFile());
 			image = Scalr.resize(image, dim);
-			g2d.drawImage(image, x + (dim - image.getWidth()) / 2, y + (dim - image.getHeight()) / 2, image.getWidth(), image.getHeight(), null);
+			switch(mode){
+				case STRETCH -> g2d.drawImage(image, x, y, dim, dim, null);
+				case KEEP_ASPECT_RATIO -> g2d.drawImage(image, x + (dim - image.getWidth()) / 2, y + (dim - image.getHeight()) / 2, image.getWidth(), image.getHeight(), null);
+			}
 		}
 		catch(IOException e){
 			Log.getLogger(null).error("Failed to read trombinoscope picture {}", picture.getPath(), e);
 		}
-	}
-	
-	@Override
-	public @NonNull AccessLevel getAccessLevel(){
-		return AccessLevel.MODERATOR;
 	}
 	
 	@NonNull
@@ -95,7 +105,7 @@ class WallCommand extends BasicCommand{
 	@NonNull
 	@Override
 	public List<String> getCommandStrings(){
-		return List.of("wall", "w");
+		return List.of("global", "g");
 	}
 	
 	@NonNull
