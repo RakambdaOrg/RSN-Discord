4b05a7b762edd50cb5fe0f40a11d5cdf5066b34f
Thomas Couchoud <thomas.couchoud@gmail.com>
2020-08-16 20:51:27 +0200

    Fix NPE

--- src/main/java/fr/raksrinana/rsndiscord/utils/log/Log.java
+++ src/main/java/fr/raksrinana/rsndiscord/utils/log/Log.java
@@ -4,19 +4,19 @@ import lombok.NonNull;
 import net.dv8tion.jda.api.entities.Guild;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Log{
-	private static final HashMap<Guild, Logger> LOGGERS = new HashMap<>();
+	private static final Map<Guild, Logger> LOGGERS = new ConcurrentHashMap<>();
+	private static final Logger NO_GUILD = LoggerFactory.getLogger("No Guild");
 	
 	@NonNull
 	public static Logger getLogger(final Guild g){
-		return LOGGERS.computeIfAbsent(g, g2 -> {
-			if(Objects.nonNull(g2)){
-				return LoggerFactory.getLogger(g2.getName());
-			}
-			return LoggerFactory.getLogger("No Guild");
-		});
+		if(Objects.isNull(g)){
+			return NO_GUILD;
+		}
+		return LOGGERS.computeIfAbsent(g, g2 -> LoggerFactory.getLogger(g2.getName()));
 	}
 }
