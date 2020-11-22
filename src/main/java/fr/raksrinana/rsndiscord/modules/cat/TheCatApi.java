7102a40e81438eb7df00aec22263282ccbb628b2
Thomas Couchoud <thomas.couchoud@gmail.com>
2020-11-22 12:03:30 +0100

    Refactor on Cat

--- src/main/java/fr/raksrinana/rsndiscord/modules/cat/TheCatApi.java
+++ src/main/java/fr/raksrinana/rsndiscord/modules/cat/TheCatApi.java
@@ -4,37 +4,44 @@ import fr.raksrinana.rsndiscord.log.Log;
 import fr.raksrinana.rsndiscord.modules.cat.data.Cat;
 import fr.raksrinana.rsndiscord.utils.RequestException;
 import fr.raksrinana.rsndiscord.utils.Utilities;
-import fr.raksrinana.utils.http.requestssenders.get.ObjectGetRequestSender;
 import kong.unirest.GenericType;
 import kong.unirest.Unirest;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import static java.util.Objects.isNull;
 
 public class TheCatApi{
 	public static final String API_URL = "https://api.thecatapi.com/v1";
 	private static String accessToken;
 	
 	private static List<Cat> getRandomCats() throws RequestException{
-		final var handler = new ObjectGetRequestSender<>(new GenericType<List<Cat>>(){}, Unirest.get(API_URL + "/images/search")
+		var request = Unirest.get(API_URL + "/images/search")
 				.headers(getHeaders())
-				.queryString("order", "RANDOM")).getRequestHandler();
-		handler.getResult().getParsingError().ifPresent(error -> {
+				.queryString("order", "RANDOM")
+				.asObject(new GenericType<List<Cat>>(){});
+		
+		request.getParsingError().ifPresent(error -> {
 			Utilities.reportException("Failed to parse TheCatAPI response", error);
 			Log.getLogger(null).warn("Failed to parse TheCatAPI response", error);
 		});
-		if(handler.getStatus() == 200){
-			return handler.getRequestResult();
+		
+		var status = request.getStatus();
+		if(status == 200){
+			return request.getBody();
 		}
-		throw new RequestException("Error sending API request, HTTP code " + handler.getStatus() + " => " + handler.getRequestResult().toString(), handler.getStatus());
+		throw new RequestException("Error sending API request, HTTP code " + status + " => " + request.getBody(), status);
 	}
 	
 	private static Map<String, String> getHeaders(){
-		final var headers = new HashMap<String, String>();
+		var headers = new HashMap<String, String>();
 		headers.put("x-api-key", getAccessToken());
 		return headers;
 	}
 	
 	public static String getAccessToken(){
-		if(Objects.isNull(accessToken)){
+		if(isNull(accessToken)){
 			accessToken = System.getProperty("THE_CAT_API_TOKEN");
 		}
 		return accessToken;
