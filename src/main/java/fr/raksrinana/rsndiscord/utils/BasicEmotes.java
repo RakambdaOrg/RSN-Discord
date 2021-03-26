ac4a6f04f8f1889dc902007a18a824d4898ba5de
Thomas Couchoud <thomas.couchoud@gmail.com>
2021-03-26 14:47:14 +0100

    fix wring BasicEmote being recognized

--- src/main/java/fr/raksrinana/rsndiscord/utils/BasicEmotes.java
+++ src/main/java/fr/raksrinana/rsndiscord/utils/BasicEmotes.java
@@ -3,39 +3,41 @@ package fr.raksrinana.rsndiscord.utils;
 import org.jetbrains.annotations.NotNull;
 
 public enum BasicEmotes implements Comparable<BasicEmotes>{
-	A("\uD83C\uDDE6", "a"),
-	B("\uD83C\uDDE7", "b"),
-	C("\uD83C\uDDE8", "c"),
-	CHECK_OK("✅"), CROSS_NO("❌"),
-	D("\uD83C\uDDE9", "d"),
-	E("\uD83C\uDDEA", "e"),
-	ERROR(""), F("\uD83C\uDDEB", "f"),
-	G("\uD83C\uDDEC", "g"),
-	H("\uD83C\uDDED", "h"),
-	I("\uD83C\uDDEE", "i"),
-	J("\uD83C\uDDEF", "j"),
-	K("\uD83C\uDDF0", "k"),
-	L("\uD83C\uDDF1", "l"),
-	M("\uD83C\uDDF2", "m"),
-	N("\uD83C\uDDF3", "n"),
-	O("\uD83C\uDDF4", "o"),
+	A("\uD83C\uDDE6", "a", "A"),
+	B("\uD83C\uDDE7", "b", "B"),
+	C("\uD83C\uDDE8", "c", "C"),
+	CHECK_OK("✅"),
+	CROSS_NO("❌"),
+	D("\uD83C\uDDE9", "d", "D"),
+	E("\uD83C\uDDEA", "e", "E"),
+	ERROR(""),
+	F("\uD83C\uDDEB", "f", "F"),
+	G("\uD83C\uDDEC", "g", "G"),
+	H("\uD83C\uDDED", "h", "H"),
+	I("\uD83C\uDDEE", "i", "I"),
+	J("\uD83C\uDDEF", "j", "J"),
+	K("\uD83C\uDDF0", "k", "K"),
+	L("\uD83C\uDDF1", "l", "L"),
+	M("\uD83C\uDDF2", "m", "M"),
+	N("\uD83C\uDDF3", "n", "N"),
+	O("\uD83C\uDDF4", "o", "O"),
 	OK_HAND("👌"),
-	P("\uD83C\uDDF5", "p"),
-	PACKAGE("\uD83D\uDCE6"),
+	P("\uD83C\uDDF5", "p", "P"),
+	PACKAGE("📦", "\uD83D\uDCE6"),
 	PAPERCLIP("\uD83D\uDCCE"),
-	Q("\uD83C\uDDF6", "q"),
-	R("\uD83C\uDDF7", "r"),
+	Q("\uD83C\uDDF6", "q", "Q"),
+	R("\uD83C\uDDF7", "r", "R"),
 	RIGHT_ARROW_CURVING_LEFT("↩️"),
-	S("\uD83C\uDDF8", "s"),
-	T("\uD83C\uDDF9", "t"),
+	S("\uD83C\uDDF8", "s", "S"),
+	T("\uD83C\uDDF9", "t", "T"),
 	THUMB_DOWN("\uD83D\uDC4E"),
 	THUMB_UP("\uD83D\uDC4D"),
-	U("\uD83C\uDDFA", "u"),
-	V("\uD83C\uDDFB", "v"),
-	W("\uD83C\uDDFC", "w"),
-	X("\uD83C\uDDFD", "x"),
-	Y("\uD83C\uDDFE", "y"),
-	Z("\uD83C\uDDFF", "z");
+	U("\uD83C\uDDFA", "u", "U"),
+	V("\uD83C\uDDFB", "v", "V"),
+	W("\uD83C\uDDFC", "w", "W"),
+	X("\uD83C\uDDFD", "x", "X"),
+	Y("\uD83C\uDDFE", "y", "Y"),
+	Z("\uD83C\uDDFF", "z", "Z");
 	private final String name;
 	private final String[] others;
 	
@@ -70,11 +72,11 @@ public enum BasicEmotes implements Comparable<BasicEmotes>{
 	@NotNull
 	public static BasicEmotes getEmote(@NotNull String text){
 		for(var emote : BasicEmotes.values()){
-			if(emote.getValue().equalsIgnoreCase(text)){
+			if(emote.getValue().equals(text)){
 				return emote;
 			}
 			for(var other : emote.getOthers()){
-				if(other.equalsIgnoreCase(text)){
+				if(other.equals(text)){
 					return emote;
 				}
 			}
