b236e749946c89a49fcd83deffd54c09b616f919
Thomas Couchoud <thomas.couchoud@gmail.com>
2021-04-18 20:39:22 +0200

    Connect to twitch securely & retry getting channel info

--- src/main/java/fr/raksrinana/rsndiscord/api/irc/TwitchUtils.java
+++ src/main/java/fr/raksrinana/rsndiscord/api/irc/TwitchUtils.java
@@ -1,18 +1,15 @@
 package fr.raksrinana.rsndiscord.api.irc;
 
-// import net.engio.mbassy.listener.Handler;
-//
-// import org.kitteh.irc.client.library.Client;
-// import org.kitteh.irc.client.library.event.channel.ChannelJoinEvent;
 import fr.raksrinana.rsndiscord.log.Log;
 import fr.raksrinana.rsndiscord.settings.Settings;
 import fr.raksrinana.rsndiscord.settings.types.ChannelConfiguration;
 import net.dv8tion.jda.api.entities.Guild;
 import org.kitteh.irc.client.library.Client;
+import org.kitteh.irc.client.library.element.Channel;
 import org.kitteh.irc.client.library.feature.twitch.TwitchSupport;
 import java.util.Objects;
 import java.util.Optional;
-import static org.kitteh.irc.client.library.Client.Builder.Server.SecurityType.INSECURE;
+import static org.kitteh.irc.client.library.Client.Builder.Server.SecurityType.SECURE;
 
 public class TwitchUtils{
 	private static final String TWITCH_IRC_HOST = "irc.chat.twitch.tv";
@@ -28,11 +25,30 @@ public class TwitchUtils{
 				.orElseThrow(() -> new RuntimeException("Twitch connection isn't enabled"));
 		
 		client.addChannel(ircChannelName);
-		var ircChannel = client.getChannel(ircChannelName)
-				.orElseThrow(() -> new RuntimeException("Couldn't get channel info"));
+		var ircChannel = tryGettingChannel(ircChannelName, 40);
 		
 		Log.getLogger(guild).info("Added Twitch listener for channel {}", ircChannelName);
 		listener.addListener(ircChannel, new GuildTwitchListener(guild.getIdLong(), channelId));
+	}
+	
+	private static Channel tryGettingChannel(String name, int attemptsLeft){
+		var channel = getIrcClient().getChannel(name);
+		if(channel.isPresent()){
+			return channel.get();
+		}
+		
+		if(attemptsLeft <= 0){
+			throw new RuntimeException("Couldn't get channel info");
+		}
+		
+		try{
+			Thread.sleep(250);
+		}
+		catch(InterruptedException e){
+			Log.getLogger().warn("Error while sleeping waiting for IRC channel", e);
+		}
+		
+		return tryGettingChannel(name, attemptsLeft - 1);
 	}
 	
 	public static void disconnect(Guild guild, String channel){
@@ -65,7 +81,7 @@ public class TwitchUtils{
 			
 			ircClient = Client.builder()
 					.server()
-					.host(TWITCH_IRC_HOST).port(6667, INSECURE)
+					.host(TWITCH_IRC_HOST).port(443, SECURE)
 					.password(getPassword()).then()
 					.nick(getUsername())
 					.build();
@@ -75,6 +91,7 @@ public class TwitchUtils{
 			
 			listener = new TwitchIRCEventListener();
 			ircClient.getEventManager().registerEventListener(listener);
+			Log.getLogger().info("IRC Client created");
 		}
 		
 		return ircClient;
