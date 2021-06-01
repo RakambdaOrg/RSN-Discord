0dcb1d77a77e48a5ab8edc7eec35eb9403f3fb5c
Thomas Couchoud <thomas.couchoud@gmail.com>
2021-06-01 21:36:24 +0200

    fix creator permission

--- src/main/java/fr/raksrinana/rsndiscord/command2/permission/CreatorPermission.java
+++ src/main/java/fr/raksrinana/rsndiscord/command2/permission/CreatorPermission.java
@@ -3,7 +3,7 @@ package fr.raksrinana.rsndiscord.command2.permission;
 import lombok.NoArgsConstructor;
 import net.dv8tion.jda.api.entities.Member;
 import org.jetbrains.annotations.NotNull;
-import static fr.raksrinana.rsndiscord.utils.Utilities.isModerator;
+import static fr.raksrinana.rsndiscord.utils.Utilities.isCreator;
 
 @NoArgsConstructor
 public class CreatorPermission implements IPermission{
@@ -11,6 +11,6 @@ public class CreatorPermission implements IPermission{
 
 	@Override
 	public boolean isAllowed(@NotNull String id, @NotNull Member member){
-		return isModerator(member);
+		return isCreator(member);
 	}
 }
