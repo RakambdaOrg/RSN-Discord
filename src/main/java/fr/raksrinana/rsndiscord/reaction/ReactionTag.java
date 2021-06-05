26083e26b413ada9ad1a7cfaa5b1278f658415bb
Thomas Couchoud <thomas.couchoud@gmail.com>
2021-06-05 17:33:26 +0200

    Add back reaction tag

--- src/main/java/fr/raksrinana/rsndiscord/reaction/ReactionTag.java
+++ src/main/java/fr/raksrinana/rsndiscord/reaction/ReactionTag.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import org.jetbrains.annotations.NotNull;
 
 public enum ReactionTag{
+	@Deprecated
+	ANILIST_TODO,
 	DELETE_CHANNEL,
 	@Deprecated
 	EXTERNAL_TODO,
