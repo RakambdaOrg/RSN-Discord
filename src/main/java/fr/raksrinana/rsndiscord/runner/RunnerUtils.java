9a606b451232e634e30262c208a2e05f7f411d83
Thomas Couchoud <thomas.couchoud@gmail.com>
2021-06-06 09:52:50 +0200

    Use default constructor

--- src/main/java/fr/raksrinana/rsndiscord/runner/RunnerUtils.java
+++ src/main/java/fr/raksrinana/rsndiscord/runner/RunnerUtils.java
@@ -5,14 +5,13 @@ import fr.raksrinana.rsndiscord.runner.api.IScheduledRunner;
 import fr.raksrinana.rsndiscord.runner.api.ScheduledRunner;
 import fr.raksrinana.rsndiscord.runner.api.ScheduledRunnerWrapper;
 import lombok.extern.log4j.Log4j2;
-import net.dv8tion.jda.api.JDA;
 import static fr.raksrinana.rsndiscord.utils.Utilities.getAllAnnotatedWith;
 
 @Log4j2
 public class RunnerUtils{
 	public static void registerAllScheduledRunners(){
 		log.info("Creating schedule runners");
-		getAllAnnotatedWith(ScheduledRunner.class, clazz -> (IScheduledRunner) clazz.getConstructor(JDA.class).newInstance(Main.getJda()))
+		getAllAnnotatedWith(ScheduledRunner.class, clazz -> (IScheduledRunner) clazz.getConstructor().newInstance())
 				.peek(c -> log.info("Loaded scheduled runner {}", c.getClass().getName()))
 				.forEach(scheduledRunner -> Main.getExecutorService().scheduleAtFixedRate(
 						new ScheduledRunnerWrapper(scheduledRunner, Main.getJda()),
