bb638290ced27ccca1c32ed7a1d0182364cd62db
Thomas Couchoud <thomas.couchoud@gmail.com>
2021-08-17 22:28:50 +0200

    Fix anilist register

--- src/main/java/fr/raksrinana/rsndiscord/command/impl/external/anilist/RegisterCommand.java
+++ src/main/java/fr/raksrinana/rsndiscord/command/impl/external/anilist/RegisterCommand.java
@@ -8,10 +8,16 @@ import fr.raksrinana.rsndiscord.utils.Utilities;
 import fr.raksrinana.rsndiscord.utils.jda.JDAWrappers;
 import lombok.extern.log4j.Log4j2;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.jetbrains.annotations.NotNull;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 import static fr.raksrinana.rsndiscord.command.CommandResult.FAILED;
 import static fr.raksrinana.rsndiscord.command.CommandResult.HANDLED;
 import static fr.raksrinana.rsndiscord.utils.LangUtils.translate;
+import static net.dv8tion.jda.api.interactions.commands.OptionType.STRING;
 
 @Log4j2
 public class RegisterCommand extends SubCommand{
@@ -30,15 +36,22 @@ public class RegisterCommand extends SubCommand{
 	}
 	
 	@Override
+	protected @NotNull Collection<? extends OptionData> getOptions(){
+		return List.of(new OptionData(STRING, TOKEN_OPTION_ID, "Token").setRequired(false));
+	}
+	
+	@Override
 	@NotNull
 	public CommandResult execute(@NotNull SlashCommandEvent event){
 		var guild = event.getGuild();
 		
-		var token = event.getOption(TOKEN_OPTION_ID).getAsString();
+		var token = Optional.ofNullable(event.getOption(TOKEN_OPTION_ID)).map(OptionMapping::getAsString);
 		
 		try{
-			AniListApi.requestToken(event.getMember(), token);
-			JDAWrappers.edit(event, translate(guild, "anilist.api-code.saved")).submitAndDelete(10);
+			token.ifPresentOrElse(code -> {
+				AniListApi.requestToken(event.getMember(), code);
+				JDAWrappers.edit(event, translate(guild, "anilist.api-code.saved")).submitAndDelete(10);
+			}, () -> JDAWrappers.edit(event, "API token can be obtained from: %s".formatted(AniListApi.getCODE_LINK())).submit());
 		}
 		catch(IllegalArgumentException e){
 			JDAWrappers.edit(event, translate(guild, "anilist.api-code.invalid")).submitAndDelete(10);
