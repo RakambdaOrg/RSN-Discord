e6ad7ad37a2c3a716367c258029750f52fc82ba3
Thomas Couchoud <1688389+RakSrinaNa@users.noreply.github.com>
2022-07-28 22:02:02 +0200

    Fix impl

--- src/main/java/fr/raksrinana/rsndiscord/interaction/command/user/base/SimpleUserCommand.java
+++ src/main/java/fr/raksrinana/rsndiscord/interaction/command/user/base/SimpleUserCommand.java
@@ -1,12 +1,11 @@
 package fr.raksrinana.rsndiscord.interaction.command.user.base;
 
-import fr.raksrinana.rsndiscord.interaction.command.slash.api.ISimpleSlashCommand;
-import fr.raksrinana.rsndiscord.interaction.command.slash.base.ExecutableSlashCommand;
+import fr.raksrinana.rsndiscord.interaction.command.user.api.ISimpleUserCommand;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import org.jetbrains.annotations.NotNull;
 
-public abstract class SimpleUserCommand extends ExecutableSlashCommand implements ISimpleSlashCommand{
+public abstract class SimpleUserCommand extends ExecutableUserCommand implements ISimpleUserCommand{
 	@Override
 	@NotNull
 	public CommandData getDefinition(){
