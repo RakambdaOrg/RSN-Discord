bf551bab834a4f3cf27acc30b0d921af2dc72c22
Thomas Couchoud <1688389+Rakambda@users.noreply.github.com>
2025-03-23 12:23:51 +0100

    Add cards

--- src/main/java/fr/rakambda/rsndiscord/spring/interaction/slash/impl/waward/AddCommand.java
+++ src/main/java/fr/rakambda/rsndiscord/spring/interaction/slash/impl/waward/AddCommand.java
@@ -545,7 +545,7 @@ public class AddCommand implements IExecutableSlashCommandGuild{
 				new Card("Mid-Autumn Festival", 1),
 				new Card("Hanoi-Style Pho", 1),
 				new Card("Ha Long Bay Cruise", 2),
-				new Card("TBD", 2),
+				new Card("Conical Hat", 2),
 				new Card("Hue Historic Citadel", 2),
 				new Card("Rice Terraces Hike", 3),
 				new Card("Golden Bridge", 3),
